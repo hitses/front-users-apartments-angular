c7daee9b3131317db7171733b92ba83a2c2fc68e
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-05-25 13:22:20 +0200

    feat: add header component with navigation links and integrate into app component

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'header-component',
+  imports: [RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export default class HeaderComponent {}
