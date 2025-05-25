afa9e67e2903924b23c68899ae602a5c7ad3c915
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-05-25 10:32:08 +0200

    feat: add apartments and users components with routing

---
+++ src/app/apartments/apartments.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apartments',
+  imports: [],
+  templateUrl: './apartments.component.html',
+  styleUrl: './apartments.component.scss',
+})
+export default class ApartmentsComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', loadComponent: () => import('./users/users.component') },
+  {
+    path: 'apartments',
+    loadComponent: () => import('./apartments/apartments.component'),
+  },
+  { path: '**', redirectTo: 'users', pathMatch: 'full' },
+];
